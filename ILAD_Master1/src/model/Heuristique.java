01b151a2d185dd8f9dc0bba43b6fa8db96a6359e
212414 <212414@C018-06.uco.local>
2019-05-21 17:56:01 +0200

    abstract Heuristique provisoire

--- ILAD_Master1/src/model/Heuristique.java
+++ ILAD_Master1/src/model/Heuristique.java
@@ -1,5 +1,9 @@
 package model;
 
 public abstract class Heuristique {
+	
+	public abstract void intitialisation();
+	public abstract String getSolutionPathName();
+	public abstract void solve();
 
 }
